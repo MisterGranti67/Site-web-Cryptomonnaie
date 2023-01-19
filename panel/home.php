495947e3edcbf9f381ad8b7239c5fa699d4ea711
mrbaguette07 <mathsgovlog@gmail.com>
2023-01-19 23:19:38 +0100

    Architecture du menu

---
+++ panel/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mon éspace joueur - Crypto Skylord</title>
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <section class="menu">
+            <img class="image" src="https://skylord.fr/logo.png">
+            <ul>
+                <li>Acheter des cryptos</li>
+                <li>Trader</li>
+                <li>Accès aux rigs</li>
+                <li>Production d'énergie</li>
+                <li class="droite">Mon Wallet</li>
+            </ul>
+        </section>
+    </body>
+</html>
