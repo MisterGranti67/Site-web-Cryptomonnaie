367cb80543e7cb5fd3087410a2a5685cec194500
mrbaguette07 <mathsgovlog@gmail.com>
2023-01-21 23:08:38 +0100

    Style switch to directory

--- panel/home.php
+++ panel/home.php
@@ -2,345 +2,7 @@
 <html>
     <head>
         <title>Mon éspace joueur - Crypto Skylord</title>
-        <style>
-            @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-            * {
-                font-family: 'Roboto';
-            }
-            body {
-                margin: 0;
-                font-family: sans-serif;
-            }
-
-            .top {
-                position: fixed;
-                top: 0;
-                height:64px;
-                width: 100%;
-                background-color: #fff;
-            }
-
-            .top--scrolling {
-                background-color: rgba(22, 22, 22, 0.85);
-                transition: background-color .4s;
-            }
-
-            .top--open {
-                background-color: rgba(22, 22, 22, 0.85);
-                transition: background-color .4s;
-            }
-
-            .navbar {
-                display: flex;
-                justify-content: center;
-                max-width: 1440px;
-                margin: 0 auto;
-                font-size: 12px;
-                height: 64px;
-                padding: 0 15px;
-            }
-
-
-            .item {
-                color: #6A6A6A;
-                text-decoration: none;
-                line-height: 64px;
-            }
-
-            .navbar_menu{
-                background-color: transparent;
-                opacity: 0;
-                transition: background-color 0s;
-                margin-right: auto;
-            }
-
-            @media (min-width: 600px) {
-                .navbar_menu {
-                    opacity: 1;
-                    display: flex;
-                    flex-direction: row;
-                    position: relative;
-                    top: auto;
-                    
-                }
-                .navbar_menu .lien {
-                    margin-right: auto;
-                    margin-left: 24px;
-                    float: left;
-                }
-
-                .mon_wallet {
-                    margin-top: auto;
-                    margin-bottom: auto;
-                    padding-top: 8px;
-                    padding-bottom: 8px;
-                    padding-left: 12px;
-                    padding-right: 12px;
-                    background-color: #FFD97D;
-                    line-height: 29px;
-                    height: 29px;
-                    width: 82px;
-                    text-align: center;
-                    border-radius: 5px;
-                }
-
-                .navbar_menu .lien:last-child {
-                    margin-right: 0;
-                }
-
-                .navbar_menu--active {
-                    display: flex;
-                    flex-direction: row;
-                    justify-content: flex-end;
-                    position: auto;
-                    height: auto;
-                    background: transparent;
-                }
-
-                .hamburger {
-                    display: none;
-                    padding: 5px auto;
-                }
-            }
-
-            .container {
-                max-width: 1024px;
-                margin-left: auto;
-                margin-right: auto;
-            }
-
-            .informations {
-                margin-top: 64px;
-                padding-top: 44px;
-                padding-bottom: 44px;
-                width: 100%;
-                background-color: #FAFAFA;
-            }
-
-            .informations .details {
-                height: 72px;
-                margin-left: 32px;
-            }
-            .informations .details img {
-                font-size: 16px;
-                float: left;
-                border-radius: 50px;
-            }
-            .informations .more_details {
-                font-size: 16px;
-                margin-left: 14px;
-                display: inline-block;
-                float: left;
-            }
-
-            .informations .more_details h1 {
-                font-size: 16px;
-                margin-block-start: 0px;
-                margin-block-end: 0px;
-                margin-inline-start: 0px;
-                margin-inline-end: 0px;
-                font-weight: 400;
-                font-style: normal;
-            }
-
-            .informations .sous_details {
-                margin-top: 4px;
-            }
-            .informations .sous_details .coordonnes {
-                display: inline-block;
-                margin-right: 24px;
-            }
-            .informations .sous_details .coordonnes h2 {
-                color: #6D6D6D;
-                font-family: 'Roboto';
-                font-style: normal;
-                font-weight: 400;
-                font-size: 12px;
-                line-height: 14px;
-            }
-
-            .informations .sous_details .coordonnes p {
-                color: #000;
-                font-family: 'Roboto';
-                font-style: normal;
-                font-weight: 400;
-                font-size: 12px;
-                line-height: 14px;
-            }
-
-            .flex-container {
-                margin-top: 24px;
-                display: flex;
-                flex-direction: row;
-                flex-wrap: nowrap;
-                justify-content: flex-start;
-                align-items: stretch;
-                align-content: stretch;
-            }
-            
-            .flex-items {
-                flex-grow: 0;
-                flex-shrink: 1;
-                flex-basis: auto;
-            }
-            .content .crypto {
-                width: 650px;
-                height: 496px;
-                margin-left: 32px;
-            }
-
-            .content .notification {
-                width: 374px;
-                height: 496px;
-                background-color: #FAFAFA;
-                border-radius: 10px;
-            }
-
-            .content .mon_solde h1 {
-                color: #000;
-                font-family: 'Roboto';
-                font-style: normal;
-                font-weight: 400;
-                font-size: 12px;
-                line-height: 14px;  
-            }
-
-            .content .mon_solde span {
-                font-family: 'Roboto';
-                font-style: normal;
-                font-weight: 700;
-                font-size: 12px;
-                line-height: 14px;
-                color: #000;
-            }
-            .notification h1 {
-                font-family: 'Roboto';
-                font-style: normal;
-                font-weight: 400;
-                font-size: 16px;
-                line-height: 19px;
-                color: #000000;
-                margin-top: 24px;
-                margin-bottom: 24px;
-                text-align: center;
-            }
-            .notification .notification_recu a {
-                font-family: 'Roboto';
-                font-style: normal;
-                font-weight: 400;
-                font-size: 12px;
-                line-height: 14px;
-                color: #000000;
-                text-align: left;
-                text-decoration: none;
-            }
-
-            .notification .notification_recu a:hover {
-                color: #6D6D6D;
-            }
-
-            .notification .notification_recu {
-                margin-bottom: 12px;
-                margin-left: 16px;
-            }
-
-            .tableau_crypto {
-                display: block;
-                margin-top: 24px;
-                width: 100%;
-            }
-            .tableau_crypto h1 {
-                font-family: 'Roboto';
-                font-style: normal;
-                font-weight: 800;
-                font-size: 12px;
-                line-height: 14px;
-                color: #000000;
-            }
-
-            .tableau_crypto h2 {
-                font-family: 'Roboto';
-                font-style: normal;
-                font-weight: 800;
-                font-size: 12px;
-                line-height: 14px;
-                color: #6D6D6D;
-
-            }
-            .tableau_crypto .nom h1 {
-                display: flex;
-                align-items: center;
-                margin-left: 10px;
-            }
-            .tableau_crypto .nom h2 {
-                display: flex;
-                align-items: center;
-                margin-left: 5px;
-            }
-            .tableau_crypto .perte {
-                color: red;
-            }
-
-            .tableau_crypto .gain {
-                color: green;
-            }
-
-            .tableau_crypto a {
-                font-family: 'Roboto';
-                font-style: normal;
-                font-weight: 400;
-                font-size: 12px;
-                line-height: 14px;
-                color: #D3B467;
-
-                text-decoration: none;
-                display: inline-block;
-            }
-
-            .tableau_crypto img {
-                width: 32px;
-                display: inline-block;
-            }
-
-            .tableau_crypto table {
-                border-collapse: collapse;
-                margin: 0 auto;
-                padding: 0;
-                width: 100%;
-                table-layout: fixed;
-            }
-
-            .tableau_crypto table caption {
-                font-size: 55px;
-                font-weight:700;
-                color:#00000090;
-                padding: 15px;
-            }
-
-            .tableau_crypto table tr {
-                padding: 2px;
-            }
-            .tableau_crypto .thead{
-                margin-top: 24px;
-                color:#000;
-            }
-
-            .tableau_crypto table th,
-            .tableau_crypto table td {
-                padding: 4px;
-                text-align: center;
-            }
-
-            .tableau_crypto table th {
-                font-size: 11px;
-                letter-spacing: .1em;
-                text-transform: capitalize;
-            }
-
-            .tableau_crypto .nom {
-                display: flex;
-            }
-        </style>
+        <link href="./css/style-panel.css" rel="stylesheet">
     </head>
     <body>
         <div class="top">
@@ -424,7 +86,7 @@
                                     </tr>
                                     <tr>
                                         <td data-label="Nom" class="nom">
-                                            <img src="../img/crypto/btclogo.png"> 
+                                            <img src="../img/crypto/ethlogo.png"> 
                                             <h1>BTC</h1>
                                             <h2>Bitcoin</h2>
                                         </td>
@@ -443,7 +105,7 @@
                                     </tr>
                                     <tr>
                                         <td data-label="Nom" class="nom">
-                                            <img src="../img/crypto/btclogo.png"> 
+                                            <img src="../img/crypto/adalogo.png"> 
                                             <h1>BTC</h1>
                                             <h2>Bitcoin</h2>
                                         </td>
@@ -462,7 +124,7 @@
                                     </tr>
                                     <tr>
                                         <td data-label="Nom" class="nom">
-                                            <img src="../img/crypto/btclogo.png"> 
+                                            <img src="../img/crypto/dogelogo.png"> 
                                             <h1>BTC</h1>
                                             <h2>Bitcoin</h2>
                                         </td>
@@ -481,7 +143,7 @@
                                     </tr>
                                     <tr>
                                         <td data-label="Nom" class="nom">
-                                            <img src="../img/crypto/btclogo.png"> 
+                                            <img src="../img/crypto/dotlogo.png"> 
                                             <h1>BTC</h1>
                                             <h2>Bitcoin</h2>
                                         </td>
@@ -500,7 +162,7 @@
                                     </tr>
                                     <tr>
                                         <td data-label="Nom" class="nom">
-                                            <img src="../img/crypto/btclogo.png"> 
+                                            <img src="../img/crypto/bnblogo.png"> 
                                             <h1>BTC</h1>
                                             <h2>Bitcoin</h2>
                                         </td>
@@ -519,7 +181,7 @@
                                     </tr>
                                     <tr>
                                         <td data-label="Nom" class="nom">
-                                            <img src="../img/crypto/btclogo.png"> 
+                                            <img src="../img/crypto/ltclogo.png"> 
                                             <h1>BTC</h1>
                                             <h2>Bitcoin</h2>
                                         </td>
@@ -538,7 +200,7 @@
                                     </tr>
                                     <tr>
                                         <td data-label="Nom" class="nom">
-                                            <img src="../img/crypto/btclogo.png"> 
+                                            <img src="../img/crypto/xrplogo.png"> 
                                             <h1>BTC</h1>
                                             <h2>Bitcoin</h2>
                                         </td>
@@ -557,7 +219,7 @@
                                     </tr>
                                     <tr>
                                         <td data-label="Nom" class="nom">
-                                            <img src="../img/crypto/btclogo.png"> 
+                                            <img src="../img/crypto/shibalogo.png"> 
                                             <h1>BTC</h1>
                                             <h2>Bitcoin</h2>
                                         </td>
@@ -581,43 +243,43 @@
                     <div class="flex-items notification">
                         <h1>Les dernières notifications</h1>
                         <div class="notification_recu">
-                            <a href="">2023-01-18 - Votre groupe électrogène #1 n’a plus d’essence</a>
+                            <a href="">2023-01-18 - Votre groupe électrogène #1 n'a plus d'essence</a>
                         </div>
                         <div class="notification_recu">
-                            <a href="">2023-01-18 - Votre groupe électrogène #1 n’a plus d’essence</a>
+                            <a href="">2023-01-18 - Votre groupe électrogène #1 n'a plus d'essence</a>
                         </div>
                         <div class="notification_recu">
-                            <a href="">2023-01-18 - Votre groupe électrogène #1 n’a plus d’essence</a>
+                            <a href="">2023-01-18 - Votre groupe électrogène #1 n'a plus d'essence</a>
                         </div>
                         <div class="notification_recu">
-                            <a href="">2023-01-18 - Votre groupe électrogène #1 n’a plus d’essence</a>
+                            <a href="">2023-01-18 - Votre groupe électrogène #1 n'a plus d'essence</a>
                         </div>
                         <div class="notification_recu">
-                            <a href="">2023-01-18 - Votre groupe électrogène #1 n’a plus d’essence</a>
+                            <a href="">2023-01-18 - Votre groupe électrogène #1 n'a plus d'essence</a>
                         </div>
                         <div class="notification_recu">
-                            <a href="">2023-01-18 - Votre groupe électrogène #1 n’a plus d’essence</a>
+                            <a href="">2023-01-18 - Votre groupe électrogène #1 n'a plus d'essence</a>
                         </div>
                         <div class="notification_recu">
-                            <a href="">2023-01-18 - Votre groupe électrogène #1 n’a plus d’essence</a>
+                            <a href="">2023-01-18 - Votre groupe électrogène #1 n'a plus d'essence</a>
                         </div>
                         <div class="notification_recu">
-                            <a href="">2023-01-18 - Votre groupe électrogène #1 n’a plus d’essence</a>
+                            <a href="">2023-01-18 - Votre groupe électrogène #1 n'a plus d'essence</a>
                         </div>
                         <div class="notification_recu">
-                            <a href="">2023-01-18 - Votre groupe électrogène #1 n’a plus d’essence</a>
+                            <a href="">2023-01-18 - Votre groupe électrogène #1 n'a plus d'essence</a>
                         </div>
                         <div class="notification_recu">
-                            <a href="">2023-01-18 - Votre groupe électrogène #1 n’a plus d’essence</a>
+                            <a href="">2023-01-18 - Votre groupe électrogène #1 n'a plus d'essence</a>
                         </div>
                         <div class="notification_recu">
-                            <a href="">2023-01-18 - Votre groupe électrogène #1 n’a plus d’essence</a>
+                            <a href="">2023-01-18 - Votre groupe électrogène #1 n'a plus d'essence</a>
                         </div>
                         <div class="notification_recu">
-                            <a href="">2023-01-18 - Votre groupe électrogène #1 n’a plus d’essence</a>
+                            <a href="">2023-01-18 - Votre groupe électrogène #1 n'a plus d'essence</a>
                         </div>
                         <div class="notification_recu">
-                            <a href="">2023-01-18 - Votre groupe électrogène #1 n’a plus d’essence</a>
+                            <a href="">2023-01-18 - Votre groupe électrogène #1 n'a plus d'essence</a>
                         </div>
                         
                     </div>
