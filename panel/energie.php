0142c35ca2ad9e0fc6eeec84d62e8830a4adecfb
mrbaguette07 <mathsgovlog@gmail.com>
2023-01-23 10:39:12 +0100

    Class active dans le menu

--- panel/energie.php
+++ panel/energie.php
@@ -45,7 +45,7 @@
                     <a class="item lien" href="">Acheter des cryptos</a>
                     <a class="item lien" href="">Trader</a>
                     <a class="item lien" href="">Accès aux rigs</a>
-                    <a class="item lien" href="">Production d'énergie</a>
+                    <a class="item lien active" href="">Production d'énergie</a>
                     <a class="item lien" href="">Technicien informatique</a>
                 </div>
                 <a class="item mon_wallet" href="">Mon Wallet</a>
