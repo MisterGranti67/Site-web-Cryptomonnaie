fdf65762c8d1f615720945ea2fe92d35eac9e4d4
mrbaguette07 <mathsgovlog@gmail.com>
2023-01-24 22:37:07 +0100

    Page de déconnexion

--- panel/home.php
+++ panel/home.php
@@ -68,24 +68,7 @@ $session_code=(isset($_SESSION['code']))?$_SESSION['code']:'';
         </script> 
     </head>
     <body>
-    <div class="top">
-        <div class="navbar">
-            <a href="" class="item logo"><img class="image" src="https://skylord.fr/logo.png" width="64px"></a>
-            <div class="navbar_menu">
-                <a class="item lien" href="">Acheter des cryptos</a>
-                <a class="item lien" href="">Trader</a>
-                <a class="item lien" href="">Accès aux rigs</a>
-                <a class="item lien" href="">Production d'énergie</a>
-                <a class="item lien" href="">Technicien informatique</a>
-            </div>
-            <a class="item mon_wallet" href="">Mon Wallet</a>
-            <span class="hamburger hamburger--collapse item" id="js-navbar-toggle">
-                <span class="hamburger-box">
-                    <span class="hamburger-inner"></span>
-                </span>
-            </span>  
-        </div>
-    </div>
+        <?php include '../utils/header.php'; ?>
         <div class="informations">
             <div class="container">
                 <div class="details">
@@ -199,7 +182,9 @@ $session_code=(isset($_SESSION['code']))?$_SESSION['code']:'';
             </div>
         </section>
     </body>
-<?php include '../utils/footer.php'; ?>
+
+    <?php include '../utils/footer.php'; ?>
+
 </html>
 <?php } else {
 header("Location: /index.php");
--- utils/header.php
+++ utils/header.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <div class="top">
     <div class="navbar">
         <a href="" class="item logo"><img class="image" src="https://skylord.fr/logo.png" width="64px"></a>
@@ -8,6 +9,9 @@
             <a class="item lien" href="">Production d'énergie</a>
             <a class="item lien" href="">Technicien informatique</a>
         </div>
+        <?php if ((isset($_SESSION['pseudo'])) && (isset($_SESSION['code']))) { ?>
+            <a class="item mon_wallet" href="https://crypto.skylord.fr/deconnexion.php">Déconnexion</a>
+        <?php } ?>
         <a class="item mon_wallet" href="">Mon Wallet</a>
         <span class="hamburger hamburger--collapse item" id="js-navbar-toggle">
             <span class="hamburger-box">
