3df4a63726c20779a34385aa85ef23e6ee971953
mrbaguette07 <mathsgovlog@gmail.com>
2023-01-24 22:33:37 +0100

    Menu

--- panel/home.php
+++ panel/home.php
@@ -68,8 +68,24 @@ $session_code=(isset($_SESSION['code']))?$_SESSION['code']:'';
         </script> 
     </head>
     <body>
-        <?php include("/utils/header.php"); ?>
-
+    <div class="top">
+        <div class="navbar">
+            <a href="" class="item logo"><img class="image" src="https://skylord.fr/logo.png" width="64px"></a>
+            <div class="navbar_menu">
+                <a class="item lien" href="">Acheter des cryptos</a>
+                <a class="item lien" href="">Trader</a>
+                <a class="item lien" href="">Accès aux rigs</a>
+                <a class="item lien" href="">Production d'énergie</a>
+                <a class="item lien" href="">Technicien informatique</a>
+            </div>
+            <a class="item mon_wallet" href="">Mon Wallet</a>
+            <span class="hamburger hamburger--collapse item" id="js-navbar-toggle">
+                <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                </span>
+            </span>  
+        </div>
+    </div>
         <div class="informations">
             <div class="container">
                 <div class="details">
