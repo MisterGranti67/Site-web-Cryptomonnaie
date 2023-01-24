40b657be05f846d2cf623b29f7f09f0816e5a113
mrbaguette07 <mathsgovlog@gmail.com>
2023-01-24 22:29:36 +0100

    Menu de la page

---
+++ utils/header.php
@@ -0,0 +1,18 @@
+<div class="top">
+    <div class="navbar">
+        <a href="" class="item logo"><img class="image" src="https://skylord.fr/logo.png" width="64px"></a>
+        <div class="navbar_menu">
+            <a class="item lien" href="">Acheter des cryptos</a>
+            <a class="item lien" href="">Trader</a>
+            <a class="item lien" href="">Accès aux rigs</a>
+            <a class="item lien" href="">Production d'énergie</a>
+            <a class="item lien" href="">Technicien informatique</a>
+        </div>
+        <a class="item mon_wallet" href="">Mon Wallet</a>
+        <span class="hamburger hamburger--collapse item" id="js-navbar-toggle">
+            <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+            </span>
+        </span>  
+    </div>
+</div>
