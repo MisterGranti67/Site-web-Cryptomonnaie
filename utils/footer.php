c8b698aa4e7cf49f7662d619cc7c902cb85ec34e
mrbaguette07 <mathsgovlog@gmail.com>
2023-01-24 22:34:44 +0100

    Include footer

---
+++ utils/footer.php
@@ -0,0 +1,39 @@
+<footer>
+    <div class="footer-menu">
+        <div class="container">
+            <div class="total-haut-footer">
+                <div class="gauche">
+                    <a href="https://skylord.fr">
+                        <img src="https://skylord.fr/logo.png">
+                    </a>
+                </div>
+                <div class="droite">
+                    <ul class="menu">
+                        <li>Nos CGU</li>
+                        <li>Nos CGV</li>
+                        <li>Mentions légales</li>
+                        <li>Vote et Gagne</li>
+                        <li>Boutique</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bas-footer">
+        <div class="container">
+            <div class="total-bas-footer">
+                <div class="gauche">
+                    <h1>Skylord</h1>
+                    <p>©(Copyright) 2023. Tous les droits réservés</p>
+                </div>
+                <div class="droite">
+                    <ul class="menu">
+                        <li><a href="https://www.tiktok.com/@play.skylord.fr"><i class="fa-brands fa-tiktok"></i></a></li>
+                        <li><a href="https://twitter.com/SkylordFR"><i class="fa-brands fa-twitter"></i></a></li>
+                        <li><a href="https://discord.gg/skylord"><i class="fa-brands fa-discord"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
