415d2fc44bce4e48fd2d51e78345853e9d7a8c28
mrbaguette07 <mathsgovlog@gmail.com>
2023-01-25 18:00:18 +0100

    Hover deconnexion

--- utils/header.php
+++ utils/header.php
@@ -10,7 +10,7 @@
             <a class="item lien" href="">Technicien informatique</a>
         </div>
         <?php if ((isset($_SESSION['pseudo'])) && (isset($_SESSION['code']))) { ?>
-            <a class="item mon_wallet" href="https://crypto.skylord.fr/deconnexion.php">Déconnexion</a>
+            <a class="item mon_wallet deconnexion" href="https://crypto.skylord.fr/deconnexion.php">Déconnexion</a>
         <?php } ?>
         <a class="item mon_wallet" href="">Mon Wallet</a>
         <span class="hamburger hamburger--collapse item" id="js-navbar-toggle">
