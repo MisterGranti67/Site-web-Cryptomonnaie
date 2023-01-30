076153ee1a6fa062877fd08c3fea574e250cad4f
mrbaguette07 <mathsgovlog@gmail.com>
2023-01-30 21:12:09 +0100

    Actualisation automatique

--- panel/home.php
+++ panel/home.php
@@ -64,11 +64,63 @@ $session_code=(isset($_SESSION['code']))?$_SESSION['code']:'';
                         }
                     } 
                 } 
+
+                actualisation_all();
+
+                async function actualisation_all() {
+                    var mon_pseudo='<?php echo $session_pseudo; ?>'
+                    var mon_code='<?php echo $session_code; ?>'
+                    $('#tableau_crypto').innerHTML('');
+                    if (mon_pseudo != '') {
+                        if (mon_code != '') {
+                            var pseudo = 'https://apiv1.skylord.fr/api/crypto?pseudo=' + mon_pseudo + '&code=' + mon_code;
+                            const api_crypto = await fetch(pseudo);
+                            const api_binance = await fetch('https://api.binance.com/api/v1/ticker/24hr');
+
+                            const data = await api_binance.json();
+                            const data_crypto = await api_crypto.json();
+
+                            var baseToText = { BTC: "Bitcoin", ETH: "Ethereum", LTC: "LiteCoin", SHIB: "Shiba", DOGE: "DogeCoin", XRP: "Ripple", DOT: "Polkadot", BNB: "BinanceCoin", ADA: "Cardano" };
+                            var baseToNumber7Days = { BTC: "1", ETH: "279", LTC: "2", SHIB: "11939", DOGE: "5", XRP: "44", DOT: "12171", BNB: "825", ADA: "975"}
+                            var valeur_solde = 0;
+                            if (data_crypto["Acces"] == "True"){
+                                for (let i = 2000; i > 0; i--) {
+                                    if ((data[i].symbol == "BTCUSDT") || (data[i].symbol == "ETHUSDT") || (data[i].symbol == "SHIBUSDT") || (data[i].symbol == "LTCUSDT") || (data[i].symbol == "DOGEUSDT") || (data[i].symbol == "XRPUSDT") || (data[i].symbol == "DOTUSDT") || (data[i].symbol == "BNBUSDT") || (data[i].symbol == "ADAUSDT")){
+                                        const nom_crypto = data[i].symbol.split('USDT');
+                                        
+                                        var variation = "perte";
+                                        if (data[i].priceChangePercent > 0) {
+                                            variation = "gain";
+                                        }
+
+                                        valeur_solde = valeur_solde+(data_crypto[nom_crypto[0]]*data[i].lastPrice)
+                                        var nom_crypto_img = nom_crypto[0].toLowerCase();
+                                        var valeur_crypto = data_crypto[nom_crypto[0]]*data[i].lastPrice
+                                        var valeur_crypto = valeur_crypto.toString().split('.');
+                                        var tableau_crypto = "<tr><td data-label=\"Nom\" class=\"nom\"><img src=\"../img/crypto/" + nom_crypto_img + "logo.png\"> <h1>" + nom_crypto[0] +"</h1><h2>" + baseToText[nom_crypto[0]] + "</h2></td><td data-label=\"Montant\"><h1>" + data_crypto[nom_crypto[0]] + "</h1></td><td data-label=\"Valeur\"><h1>≈" + valeur_crypto[0] + " $</h1></td><td data-label=\"Variation sur 24h\"><h1 class=\" " + variation + " \"> " + data[i].priceChangePercent + "%<h1></td><td data-label=\"Les 7 derniers jours\"><img src=\"https://www.coingecko.com/coins/" + baseToNumber7Days[nom_crypto[0]] + "/sparkline\" style=\"width:96px\" /></td><td data-label=\" \"><a href=\"\">Trader</a></td></tr>"
+                                        $(tableau_crypto).prependTo("#tableau_crypto");
+                                        
+                                    }
+                                    
+                                }
+                                document.getElementById("solde").textContent = "≈" + valeur_solde + "$";
+                                document.getElementById("info_pseudo").textContent = mon_pseudo;
+                                document.getElementById("info_connexion").textContent = data_crypto["Date_heure"];
+                                var valeur_img = "https://minotar.net/avatar/" + mon_pseudo;
+                                $("#info_avatar").attr('src', valeur_img);
+                            } else {
+                                document.body.innerHTML = "<p>ERREUR, Vous n'êtes plus connecté.</p>"; 
+                            }
+                        } 
+                    } 
+                    setTimeout(actualisation_all,10000);
+                    actualisation_all();
+
+                }
+
             });
-
-
             
-        </script> 
+        </script>
     </head>
     <body>
         <div id="chargement" class="chargement">
